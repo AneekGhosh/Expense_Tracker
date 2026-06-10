a6411a4ef7f21d20b5157f71d05207f592109494
Aneek Ghosh <ghoshaneek02@gmail.com>
2026-06-11 00:58:11 +0530

    Add axios dependency

--- expense-tracker-frontend/src/main.jsx
+++ expense-tracker-frontend/src/main.jsx
@@ -1,15 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // 👉 1. Import BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // 👉 2. Wrap the entire app inside the BrowserRouter
   <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
